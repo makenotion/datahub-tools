c143ed1415122cef0c84601149c0451f7f4d98be
Ada Draginda <ada@makenotion.com>
2023-01-25 15:25:57 -0800

    add example and clean up extract_dbt_resources

---
+++ examples/annotate_sql.py
@@ -0,0 +1,157 @@
+"""
+
+This script is a demonstration of an application of this package and was generated as part of a talk at a
+DataHub Town Hall. Deck:
+https://docs.google.com/presentation/d/1m-97klgCTcMXqacH10vpj2JE6NEWhWp4z5Hh9WhAf2c/edit?usp=sharing
+
+This script reads your DBT manifest.json and adds a link to its appropriate DataHub page at the top of the
+SQL file.
+
+run with
+```
+python annotate_sql --manifest_file /path/to/your/manifest.json
+```
+
+Author: Ada Draginda <ada[at]makenotion.com>
+Date: January 25, 2023
+"""
+from __future__ import annotations
+
+import logging
+import pathlib
+from logging.config import dictConfig
+from string import Template
+from typing import Dict, List
+
+import click
+
+from datahub_tools.utils import extract_dbt_resources
+
+# Every link shares this same prefix. We specify it separately so that we can remove
+# links before writing them again (avoiding more than one link showing).
+# The reason we don't use the full DH_LINK is to support db/schema changes (e.g.
+# if we move a table)
+DH_LINK_PREFIX = "-- https://notion.acryl.io/dataset/urn:li:dataset:"
+DH_LINK = Template(
+    f"{DH_LINK_PREFIX}(urn:li:dataPlatform:dbt,$table_name,PROD)  --noqa\n"
+)
+
+
+def setup_logging(level: int = logging.INFO):
+    config = {
+        "version": 1,
+        "disable_existing_loggers": True,
+        "formatters": {
+            "standard": {
+                "format": "[%(asctime)s][%(levelname)7s][ %(name)s ]: %(message)s"
+            },
+        },
+        "handlers": {
+            "stdout": {
+                "level": level,
+                "formatter": "standard",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "loggers": {
+            "": {  # root logger
+                "handlers": ["stdout"],
+                "level": level,
+                "propagate": False,
+            },
+        },
+    }
+    dictConfig(config)
+
+
+def config_to_str(config: Dict):
+    all_str = []
+    for k, v in config.items():
+        if isinstance(v, dict):
+            _dict = ",\n            ".join([f"{_k!r}: {_v!r}" for _k, _v in v.items()])
+            to_append = f"{k} = {{\n            {_dict}\n        }}"
+        elif isinstance(v, bool):
+            to_append = f"{k} = {str(v).lower()}"
+        else:
+            to_append = f"{k} = {v!r}"
+        all_str.append(to_append)
+    joined_str = ",\n        ".join(all_str)
+    out = f"{{{{\n    config(\n        {joined_str}\n    )\n}}}}\n" if config else ""
+    return (
+        # HACK: see `get_cleaned_config` for why this bit is needed
+        out.replace(
+            "anomalo_check_offset = \"-var('mrr_look_back_days')\"",
+            "anomalo_check_offset = -var('mrr_look_back_days')",
+        )
+        # True/False works just fine, but we should be consistent across all files and
+        # lowercase is used much more often than the upper case flavors.
+        .replace(": False", ": false").replace(": True", ": true")
+    )
+
+
+@click.command()
+@click.option(
+    "--manifest_file",
+    type=click.Path(
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        path_type=pathlib.Path,
+    ),
+    help="Manifest file generated from `dbt docs --target datahub`",
+)
+@click.option(
+    "--resource_type_filter",
+    multiple=True,
+    default=None,
+    help="One or more resource_types can be included to filter dbt resources, e.g. 'model', 'snapshot', etc.",
+)
+def main(manifest_file: pathlib.Path, resource_type_filter: List[str] | None):
+    """
+    Insert a link to each model's DataHub page at the top of each SQL file
+    """
+    logger = logging.getLogger(__name__)
+    dbt_resources = extract_dbt_resources(
+        manifest_file=manifest_file, resource_type_filter=resource_type_filter
+    )
+    for unique_id, node in dbt_resources:
+        logger.info(unique_id)
+        original_file_path = node.get("original_file_path")
+        if original_file_path and original_file_path.endswith(".sql"):
+            # assemble the DataHub link
+            name = f"{node['alias'] or node['name']}"
+            table_name = f"{node['database']}.{node['schema']}.{name}"
+            datahub_link = DH_LINK.substitute(table_name=table_name)
+            logger.info(datahub_link)
+            code: str = node.get("raw_code") or node.get("raw_sql")
+
+            if not code:
+                logger.error("DBT did not produce code for: %s", original_file_path)
+                continue
+
+            # remove any existing links
+            if code.startswith(DH_LINK_PREFIX):
+                code = "\n".join(code.split("\n")[1:])
+
+            # the snapshot header and footer will not be present in the raw sql, and so we have to add them
+            # back in before writing
+            if node["resource_type"] == "snapshot":
+                wrapped_out_code = f"{{% snapshot {name} %}}\n\n{code.strip()}\n\n{{% endsnapshot %}}\n"
+            else:
+                wrapped_out_code = code
+
+            suffix = "" if wrapped_out_code.endswith("\n") else "\n"
+
+            file_path = pathlib.Path(node["root_path"]) / pathlib.Path(
+                original_file_path
+            )
+            logger.info(file_path)
+            with file_path.open(mode="w") as f:
+                f.writelines(f"{datahub_link}{wrapped_out_code}{suffix}")
+
+
+if __name__ == "__main__":
+    setup_logging()
+    main()
---
+++ examples/propagate_priorities.py
@@ -0,0 +1,178 @@
+"""
+
+This script is a demonstration of an application of this package and was generated as part of a talk at a
+DataHub Town Hall. Deck:
+https://docs.google.com/presentation/d/1m-97klgCTcMXqacH10vpj2JE6NEWhWp4z5Hh9WhAf2c/edit?usp=sharing
+
+The script takes all existing DataHub entities and insert a priority tag that is calculated based on dbt
+metadata. The script works with any data source, but was originally written with DBT resources (dicts come
+from the generated manifest.json).
+
+To get the script to run, a user has to fill out two functions (clearly annotated) :
+* calculate_priority_tag: For a given dict, return a priority (e.g. P0, P1, P2)
+* assemble_priority_metadata: For a given dict, generate metadata that you would like to propagate to DataHub
+
+Author: Ada Draginda <ada[at]makenotion.com>
+Date: January 25, 2023
+"""
+from __future__ import annotations
+
+import logging
+from pathlib import Path
+from typing import Any, Dict, List
+
+import click
+import datahub.emitter.mce_builder as builder
+
+from datahub_tools.classes import DHEntity
+from datahub_tools.client import emit_metadata, get_datahub_entities, remove_tags, set_tags
+from datahub_tools.utils import extract_dbt_resources
+
+
+def _to_priority_urn(priority: str) -> str:
+    assert len(priority) == 2
+    return builder.make_tag_urn(f"Priority: {priority}")
+
+
+# ----> REQUIRES EDITING
+def calculate_priority_tag(dbt_resource: Dict[str, Any]) -> str:
+    # insert logic for calculating a priority based on the metadata in the dbt resource
+    priority = ...
+    return builder.make_tag_urn(f"Priority: {priority}")
+
+
+# ----> REQUIRES EDITING
+def assemble_priority_metadata(dbt_resource: Dict[str, Any]) -> Dict[str, str]:
+    """
+    Given a dictionary for a given dbt resource (from manifest.json), calculate a
+    priority. The return must be a valid URN (see _to_priority_urn which can create an
+    example priority tag given a priority such as P0, P1, etc).
+    """
+    return ...
+
+
+def _set_priority_tags(
+    add_tags: Dict[str, List[DHEntity]],
+    rem_tags: Dict[str, List[DHEntity]],
+    dry_run: bool,
+):
+    logger = logging.getLogger(__name__)
+    logger.info("--- Tags to add:")
+    for tag, resources in add_tags.items():
+        for res in resources:
+            logger.info("%s : %s", tag, res.name)
+    logger.info("--- Tags to remove:")
+    for tag, resources in rem_tags.items():
+        for res in resources:
+            logger.info("%s : %s", tag, res.name)
+
+    if not dry_run:
+        # -- removals
+        for tag_urn, entities in rem_tags.items():
+            remove_tags(tag_urns=tag_urn, resource_urns=[x.urn for x in entities])
+            for entity in entities:
+                metadata = entity.metadata.copy()
+                if "priority" in metadata:
+                    metadata.pop("priority")
+                emit_metadata(metadata=metadata, resource_urn=entity.urn)
+
+        # -- adds
+        for tag_urn, entities in add_tags.items():
+            set_tags(tag_urns=tag_urn, resource_urns=[x.urn for x in entities])
+            for entity in entities:
+                metadata = entity.metadata.copy()
+                # tag_urn example: urn:li:tag:Priority: P0
+                # tag_urn.rsplit(" ")[1] ==> 'P0'
+                metadata["priority"] = tag_urn.rsplit(" ")[1]
+                emit_metadata(metadata=metadata, resource_urn=entity.urn)
+
+
+@click.command()
+@click.option(
+    "--manifest_file",
+    type=click.Path(
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        path_type=Path,
+    ),
+    help="Manifest file generated from `dbt docs`",
+)
+@click.option("--dry_run", is_flag=True, default=False)
+@click.pass_context
+def propagate_dbt_priority_metadata(manifest_file: Path, dry_run: bool):
+    """
+    Propagates dbt priority metadata flags to upstreams dbt resources
+
+    1) Calculates the priority metadata for a table based on its upstream dependencies
+    2) uses the metadata to calculate and set a priority (P0, P1, P2)
+    3) writes the metadata to datahub
+        - The 4 priority entities for the table (e.g. is reported externally)
+        - The same 4 but as calculated by upstream dependencies (e.g. has upstream
+          dependencies that are reported externally
+    """
+    logger = logging.getLogger(__name__)
+
+    dbt_resources_by_unique_id = extract_dbt_resources(manifest_file=manifest_file)
+    # we need the resources by its storage name instead of its unique_id as that is how
+    # DataHub names resources
+    dbt_resources_by_name = {
+        f"{v['database']}.{v['schema']}.{v['alias'] or v['name']}": v
+        for v in dbt_resources_by_unique_id.values()
+    }
+    entities: List[DHEntity] = get_datahub_entities()
+
+    add_tags = {}
+    rem_tags = {}
+    for dh_entity in entities:
+        dbt_resource = dbt_resources_by_name.get(dh_entity.name)
+        if not dbt_resource:
+            # the DataHub entity is not a dbt resource
+            continue
+
+        new_priority_tag_urn = calculate_priority_tag(dbt_resource)
+        existing_priority_tag_urn = _to_priority_urn(dh_entity.get_priority())
+
+        # ------
+        # Depending on the new and existing priority tags, add/remove tags as appropriate
+        if new_priority_tag_urn:
+            if existing_priority_tag_urn:
+                if new_priority_tag_urn != existing_priority_tag_urn:
+                    entities = rem_tags.get(existing_priority_tag_urn, [])
+                    entities.append(dh_entity)
+                    if existing_priority_tag_urn not in rem_tags:
+                        rem_tags[existing_priority_tag_urn] = entities
+
+                    entities = add_tags.get(new_priority_tag_urn, [])
+                    entities.append(dh_entity)
+                    if new_priority_tag_urn not in add_tags:
+                        add_tags[new_priority_tag_urn] = entities
+            else:
+                entities = add_tags.get(new_priority_tag_urn, [])
+                entities.append(dh_entity)
+                if new_priority_tag_urn not in add_tags:
+                    add_tags[new_priority_tag_urn] = entities
+        elif existing_priority_tag_urn:
+            entities = rem_tags.get(existing_priority_tag_urn, [])
+            entities.append(dh_entity)
+            if existing_priority_tag_urn not in rem_tags:
+                rem_tags[existing_priority_tag_urn] = entities
+        # ------
+
+        new_priority_metadata = assemble_priority_metadata(dbt_resource)
+        metadata = dh_entity.metadata.copy()
+        metadata.update(new_priority_metadata)
+        if dry_run:
+            logger.info(
+                "dry run: emit to %s:\n%s\nFull metadata: %s",
+                dh_entity.urn,
+                new_priority_metadata,
+                metadata,
+            )
+        else:
+            emit_metadata(metadata=metadata, resource_urn=dh_entity.urn)
+
+    logger.info("[%d] Tags to add, [%d] tags to remove", len(add_tags), len(rem_tags))
+
+    _set_priority_tags(add_tags=add_tags, rem_tags=rem_tags, dry_run=dry_run)
