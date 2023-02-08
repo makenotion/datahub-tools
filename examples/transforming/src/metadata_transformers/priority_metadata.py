a1a5e1e2b51d69a158da3a1c3b3f1f0839e3ef2f
Ada Draginda <ada@makenotion.com>
2023-02-07 18:11:37 -0800

    add transforming example along with dbt dependencies fn (#5)

---
+++ examples/transforming/src/metadata_transformers/priority_metadata.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Any, Dict, NoReturn
+
+import datahub.emitter.mce_builder as builder
+import jmespath
+from datahub_tools.dbt import (
+    ModelDependencies,
+    extract_dbt_resources,
+    get_dbt_dependencies,
+)
+
+
+@dataclass
+class PriorityMetadata:
+    unique_id: str
+    resource_name: str
+    is_reported_externally: bool
+    is_used_to_drive_company_metrics: bool
+
+    original_is_reported_externally: bool = field(init=False)
+    original_is_used_to_drive_company_metrics: bool = field(init=False)
+
+    def __post_init__(self):
+        self.original_is_reported_externally = self.is_reported_externally
+        self.original_is_used_to_drive_company_metrics = (
+            self.is_used_to_drive_company_metrics
+        )
+
+    @classmethod
+    def from_resource(cls, resource: dict) -> PriorityMetadata:
+        metadata = jmespath.search("config.notion_metadata", resource) or {}
+        table_name = f"{resource['alias'] or resource['name']}"
+        resource_name = f"{resource['database']}.{resource['schema']}.{table_name}"
+        is_reported_externally = metadata.get("is_reported_externally")
+        is_used_to_drive_company_metrics = metadata.get(
+            "is_used_to_drive_company_metrics"
+        )
+
+        return PriorityMetadata(
+            unique_id=resource["unique_id"],
+            resource_name=resource_name,
+            is_reported_externally=is_reported_externally or False,
+            is_used_to_drive_company_metrics=is_used_to_drive_company_metrics or False,
+        )
+
+    def propagate(self, other: PriorityMetadata) -> NoReturn:
+        self.is_reported_externally = (
+            self.is_reported_externally or other.is_reported_externally
+        )
+        self.is_used_to_drive_company_metrics = (
+            self.is_used_to_drive_company_metrics
+            or other.is_used_to_drive_company_metrics
+        )
+
+    def get_priority(self) -> str:
+        if self.is_reported_externally:
+            out = "P0"
+        elif self.is_used_to_drive_company_metrics:
+            out = "P1"
+        else:
+            out = "P2"
+
+        return out
+
+    def get_priority_urn(self) -> str:
+        return builder.make_tag_urn(f"Priority: {self.get_priority()}")
+
+    def get_metadata_for_datahub(self) -> dict[str, str]:
+        out = {
+            "has downstream dependencies that are reported externally": self.is_reported_externally,
+            "has downstream dependencies that are used to drive company metrics": self.is_used_to_drive_company_metrics,
+            "is reported externally": self.original_is_reported_externally,
+            "is used to drive company metrics": self.original_is_used_to_drive_company_metrics,
+        }
+        return {k: str(v) for k, v in out.items()}
+
+
+def generate_priority_metadata(
+    manifest_file: str | Path,
+) -> list[PriorityMetadata]:
+    """
+    For each resource in the given dbt model, find the priority metadata (e.g.
+    "is_reported_externally"), propagate it to upstream dependencies and then return a
+    list of each resource as a PriorityMetadata object (one object is one dbt resource).
+    :param manifest_file: dbt manifest.json generated using `dbt docs`
+    """
+    # unique_id : resource
+    dbt_resources_by_unique_id: Dict[str, Dict[str, Any]] = extract_dbt_resources(
+        manifest_file=manifest_file, resource_type_filter=["model", "snapshot"]
+    )
+    # unique_id : dependencies
+    deps: Dict[str, ModelDependencies] = get_dbt_dependencies(
+        dbt_resources_by_unique_id=dbt_resources_by_unique_id
+    )
+
+    priority_metadata_objs = {
+        unique_id: PriorityMetadata.from_resource(r)
+        for unique_id, r in dbt_resources_by_unique_id.items()
+    }
+    # propagate the metadata to all upstream tables. Since we always use 'or' when
+    # propagating the boolean flags, we'll always have `true` set for the metadata
+    # entries if the table or at least 1 downstream dep is true.
+    for unique_id, priority_metadata_obj in priority_metadata_objs.items():
+        upstream_unique_ids = deps[unique_id].get_all_upstream()
+        for upstream_unique_id in upstream_unique_ids:
+            priority_metadata_objs[upstream_unique_id].propagate(priority_metadata_obj)
+
+    return list(priority_metadata_objs.values())
---
+++ examples/transforming/src/metadata_transformers/priority_metadata_transformer.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import logging
+import os
+from pathlib import Path
+from typing import Dict
+
+import datahub.emitter.mce_builder as builder
+from datahub.ingestion.transformer.add_dataset_properties import (
+    AddDatasetPropertiesResolverBase,
+)
+from datahub.metadata.schema_classes import DatasetKeyClass
+
+from metadata_transformers.priority_metadata import (
+    PriorityMetadata,
+    generate_priority_metadata,
+)
+
+
+class PriorityPropertiesResolver(AddDatasetPropertiesResolverBase):
+    def __init__(self):
+        super().__init__()
+        dbt_target = os.environ.get("DBT_TARGET")
+        if not dbt_target:
+            raise ValueError(
+                "must specify the location of the dbt target files with the "
+                "environment variable DBT_TARGET"
+            )
+        _metadata = generate_priority_metadata(Path(dbt_target) / "manifest.json")
+        self.priority_metadata_objs: Dict[str, PriorityMetadata] = {
+            x.resource_name: x for x in _metadata
+        }
+
+    def get_properties_to_add(self, entity_urn: str) -> Dict[str, str]:
+        logger = logging.getLogger(__name__)
+        dataset_key: DatasetKeyClass = builder.dataset_urn_to_key(entity_urn)
+        priority_metadata_obj: PriorityMetadata = self.priority_metadata_objs.get(
+            dataset_key.name
+        )
+        if priority_metadata_obj:
+            priority_properties = priority_metadata_obj.get_metadata_for_datahub()
+            logger.info("urn: %s - %s", entity_urn, dataset_key.name)
+            logger.info("Adding properties: %s", priority_properties)
+        else:
+            priority_properties = {}
+        return priority_properties
