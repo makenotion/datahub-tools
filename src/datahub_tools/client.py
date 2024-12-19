6757e96045feda2e10becc5f3982ee7a01c28612
Peter Ray <137835764+praynotion@users.noreply.github.com>
2024-12-19 15:35:57 -0800

    Revert "Updating updates to check existence (#25)"
    This reverts commit 8b019c7c08d5f5e909b33d665dd1767d2ad4ec49.

--- src/datahub_tools/client.py
+++ src/datahub_tools/client.py
@@ -517,27 +517,22 @@ def update_field_descriptions(
     """
     responses = {}
     for k, v in field_descriptions.items():
-        emitter = emitter = get_dh_emitter()
-        exists = emitter.get_aspect(resource_urn)
-        if exists:
-            _input = (
-                '{ description: "' + _escape_chars(v) + '", '
-                f'resourceUrn: "{resource_urn}", '
-                f"subResourceType: DATASET_FIELD, "
-                f'subResource: "{k}" }}'
+        _input = (
+            '{ description: "' + _escape_chars(v) + '", '
+            f'resourceUrn: "{resource_urn}", '
+            f"subResourceType: DATASET_FIELD, "
+            f'subResource: "{k}" }}'
+        )
+        endpoint = "updateDescription"
+        response = _post_mutation(
+            endpoint=endpoint,
+            _input=_input,
+        )
+        if not response:
+            raise ValueError(
+                f"Failed to update field description '{k}' (but returned 200) for {resource_urn}"
             )
-            endpoint = "updateDescription"
-            response = _post_mutation(
-                endpoint=endpoint,
-                _input=_input,
-            )
-            if not response:
-                raise ValueError(
-                    f"Failed to update field description '{k}' (but returned 200) for {resource_urn}"
-                )
-            responses[k] = response[endpoint]
-        else:
-            raise ValueError(f"Failed to get entity {resource_urn}: Does not exist")
+        responses[k] = response[endpoint]
     return responses
 
 
@@ -548,25 +543,17 @@ def update_dataset_description(resource_urn: str, description: str) -> dict[str,
     :param description: The description that you want to set for the dataset/resource
     :return: Resource URN changed
     """
-    emitter = emitter = get_dh_emitter()
-    exists = emitter.get_aspect(resource_urn)
-    responses = {}
-    if exists:
-        _input = (
-            '{ editableProperties: { description: "'
-            + _escape_chars(description)
-            + '" } }'
+    _input = (
+        '{ editableProperties: { description: "' + _escape_chars(description) + '" } }'
+    )
+    endpoint = "updateDataset"
+    response = _post_mutation(
+        endpoint=endpoint, _input=_input, urn=resource_urn, subselection="urn"
+    )
+    if not response:
+        raise ValueError(
+            f"Failed to update entity descriptions (but returned 200) for {resource_urn}"
         )
-        endpoint = "updateDataset"
-        response = _post_mutation(
-            endpoint=endpoint, _input=_input, urn=resource_urn, subselection="urn"
-        )
-        if not response:
-            raise ValueError(
-                f"Failed to update entity descriptions (but returned 200) for {resource_urn}"
-            )
-    else:
-        raise ValueError(f"Failed to get entity {resource_urn}: Does not exist")
     return response[endpoint]
 
 
